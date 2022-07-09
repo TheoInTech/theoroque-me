efdd7162921dd43a9e5cac40ad74fc8837101ba7
Theo Roque <theoroque95@gmail.com>
2022-07-09 23:22:44 +0800

    change info

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -134,13 +134,13 @@ const Info = () => {
         Roque
       </NameH1>
       <PositionH2>
-        <PositionSpan>Javascript Developer</PositionSpan>
-        <PositionSpan>Graphic Designer</PositionSpan>
+        <PositionSpan>Front-end Developer</PositionSpan>
         <PositionSpan>IT Consultant</PositionSpan>
         <PositionSpan>Technopreneur</PositionSpan>
+        <PositionSpan>Web3 Enthusiast</PositionSpan>
       </PositionH2>
       <IconList>
-        <li>
+        {/* <li>
           <IconLink
             href="https://www.facebook.com/theoroque95"
             target="_blank"
@@ -157,6 +157,15 @@ const Info = () => {
               <g></g>
               <path d="M12.461 5.57l-0.309 2.93h-2.342v8.5h-3.518v-8.5h-1.753v-2.93h1.753v-1.764c0-2.383 0.991-3.806 3.808-3.806h2.341v2.93h-1.465c-1.093 0-1.166 0.413-1.166 1.176v1.464h2.651z" />
             </svg>
+          </IconLink>
+        </li> */}
+        <li>
+          <IconLink
+            href="https://www.upwork.com/freelancers/~01894d53199393d23c"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 102 28" role="img" aria-hidden="true"><path fill="#fff" d="M28.18,19.06A6.54,6.54,0,0,1,23,16c.67-5.34,2.62-7,5.2-7s4.54,2,4.54,5-2,5-4.54,5m0-13.34a7.77,7.77,0,0,0-7.9,6.08,26,26,0,0,1-1.93-5.62H12v7.9c0,2.87-1.3,5-3.85,5s-4-2.12-4-5l0-7.9H.49v7.9A8.61,8.61,0,0,0,2.6,20a7.27,7.27,0,0,0,5.54,2.35c4.41,0,7.5-3.39,7.5-8.24V8.77a25.87,25.87,0,0,0,3.66,8.05L17.34,28h3.72l1.29-7.92a11,11,0,0,0,1.36,1,8.32,8.32,0,0,0,4.14,1.28h.34A8.1,8.1,0,0,0,36.37,14a8.12,8.12,0,0,0-8.19-8.31"></path> <path fill="#fff" d="M80.8,7.86V6.18H77.2V21.81h3.65V15.69c0-3.77.34-6.48,5.4-6.13V6c-2.36-.18-4.2.31-5.45,1.87"></path> <polygon fill="#fff" points="55.51 6.17 52.87 17.11 50.05 6.17 45.41 6.17 42.59 17.11 39.95 6.17 36.26 6.17 40.31 21.82 44.69 21.82 47.73 10.71 50.74 21.82 55.12 21.82 59.4 6.17 55.51 6.17"></polygon> <path fill="#fff" d="M67.42,19.07c-2.59,0-4.53-2.05-4.53-5s2-5,4.53-5S72,11,72,14s-2,5-4.54,5m0-13.35A8.1,8.1,0,0,0,59.25,14,8.18,8.18,0,1,0,75.6,14a8.11,8.11,0,0,0-8.18-8.31"></path> <path fill="#fff" d="M91.47,14.13h.84l5.09,7.69h4.11l-5.85-8.53a7.66,7.66,0,0,0,4.74-7.11H96.77c0,3.37-2.66,4.65-5.3,4.65V0H87.82V21.82h3.64Z"></path></svg>
           </IconLink>
         </li>
         <li>
@@ -178,7 +187,7 @@ const Info = () => {
             </svg>
           </IconLink>
         </li>
-        <li>
+        {/* <li>
           <IconLink
             href="https://www.instagram.com/theoroque95"
             target="_blank"
@@ -196,7 +205,7 @@ const Info = () => {
               <path d="M13 0h-9c-2.2 0-4 1.8-4 4v9c0 2.2 1.8 4 4 4h9c2.2 0 4-1.8 4-4v-9c0-2.2-1.8-4-4-4zM16 13c0 1.654-1.346 3-3 3h-9c-1.654 0-3-1.346-3-3v-6h3.207c-0.286 0.61-0.457 1.283-0.457 2 0 2.619 2.131 4.75 4.75 4.75s4.75-2.131 4.75-4.75c0-0.717-0.171-1.39-0.457-2h3.207v6zM12.25 9c0 2.068-1.682 3.75-3.75 3.75s-3.75-1.682-3.75-3.75 1.682-3.75 3.75-3.75 3.75 1.682 3.75 3.75zM12.152 6c-0.872-1.059-2.176-1.75-3.652-1.75s-2.78 0.691-3.652 1.75h-3.848v-2c0-1.654 1.346-3 3-3h9c1.654 0 3 1.346 3 3v2h-3.848zM14.454 2.722v1.298c0 0.299-0.244 0.543-0.542 0.543h-1.368c-0.3-0.001-0.544-0.245-0.544-0.543v-1.298c0-0.299 0.244-0.543 0.544-0.543h1.368c0.298 0 0.542 0.244 0.542 0.543z" />
             </svg>
           </IconLink>
-        </li>
+        </li> */}
         <li>
           <IconLink
             href="https://stackoverflow.com/users/11580842/theo-roque"
